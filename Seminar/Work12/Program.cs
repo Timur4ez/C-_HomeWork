b23fbfd82650da60e19ddb8008981b37e2fa8c03
Timur4ez <Matchanoff@bk.ru>
2022-10-21 23:32:44 +0300

    Внёс изменения в задачу 23

--- Seminar/Work12/Program.cs
+++ Seminar/Work12/Program.cs
@@ -2,16 +2,19 @@
 //5 -> 1, 4, 9, 16, 25.
 //2 -> 1,4
 
-void PrintSquareaToConsole(int n)
+void PrintSquareaToConsole(int n);
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++);
 {
-    for (int i = 1; i <= n; i++);
-    {
-        Console.WriteLine(i * i);
-    }
+    Console.WriteLine(i * i);
 }
 
 PrintSquareaToConsole(10);
 for (int i = 1; i <=5; i++)
 {
-    Console.WriteLine(i);
+   Console.WriteLine(i);
 }
+//void PrintResult(string message)
+//{
+//    Console.Write(message);
+//}
