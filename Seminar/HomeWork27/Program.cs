c28cda5c64df81c1d95d68c78ff611a69cd68bc2
Timur4ez <Matchanoff@bk.ru>
2022-11-08 21:34:40 +0300

    Решена задача 27

--- Seminar/HomeWork27/Program.cs
+++ Seminar/HomeWork27/Program.cs
@@ -2,11 +2,13 @@
 
 Console.Write("Enter a number:");
 int number = Convert.ToInt32(Console.ReadLine());
-string strNumber = number.ToString();
-int[] array = new int[strNumber.Length];
+string strNum = number.ToString();
+int[]array = new int[strNum.Length];
 int sum = 0;
-for (int i = 0; i< strNumber.Length; i++)
+
+for (int i = 0; i < strNum.Length; i++)
 {
+    array[i] = int.Parse(strNum[i].ToString());
     sum = sum + array[i];
 }
 Console.WriteLine(sum);
