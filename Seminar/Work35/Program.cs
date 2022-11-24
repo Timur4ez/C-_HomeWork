0f141a86a7a2bc34c2c0fbaa5e5789eccd2da908
Timur4ez <Matchanoff@bk.ru>
2022-11-24 21:28:02 +0300

    Решены задачи 64 66 68

--- Seminar/Work35/Program.cs
+++ Seminar/Work35/Program.cs
@@ -1,1 +1,17 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+int Numbers(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    Console.Write(n + ", ");
+    return Numbers(n - 1);
+}
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Number = Numbers(n);
+Console.WriteLine(Number);
--- Seminar/Work37/Program.cs
+++ Seminar/Work37/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.Write("Write number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumRec(int number)
+{
+ if (number == 0) return 0;
+ else return number + SumRec(number - 1);
+}
+Console.WriteLine(SumRec(number));
